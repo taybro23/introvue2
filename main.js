614fdca766b44347a2620cc1a855145e81d661bc
Taylor Brookes <72576222+taybro23@users.noreply.github.com>
2023-02-01 12:05:28 +0000

    list rendering

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: 'vmSocks-green-onWhite.jpeg',
-        inventory: 100
+        inventory: 100,
+        details: ["80% cotton", "20% polyester", "Gender-Neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
