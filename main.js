849b75f511120d6a527416b5d497fac3a619c6fc
Taylor Brookes <72576222+taybro23@users.noreply.github.com>
2024-01-02 14:54:52 +0000

    computed properties

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
     description: 'Cool Socks',
-    image: './assets/g-socks.jpeg',
+    selectedVariant: 0,
     link: 'https://www.amazon.com',
-    inventory: 110,
-    onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColour: 'green',
-        variantImage: './assets/g-socks.jpeg'
+        variantImage: './assets/g-socks.jpeg',
+        variantQuantity: 50
       },
       {
         variantId: 2235,
         variantColour: 'blue',
-        variantImage: './assets/b-socks.jpeg'
+        variantImage: './assets/b-socks.jpeg',
+        variantQuantity: 0
       },
     ],
     sizes: ['XS', 'S', 'M', 'L', 'XL'],
@@ -32,8 +33,22 @@ var app = new Vue({
         this.cart -= 1
       }
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    onSale() {
+      return this.brand + ' ' + this.product + ' are on sale!'
     }
   }
 })
